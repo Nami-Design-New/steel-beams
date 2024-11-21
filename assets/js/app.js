0059a61a88d2f841dc2142292d8880fc0e431ec2
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-11-21 14:23:13 +0200

    first

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {});
+
+$(document).ready(function () {
+  $(".preloader").delay(1200).fadeOut(300);
+
+  if ($(window).width() > 768) {
+    $("section").each(function () {
+      const sectionDivs = $(this).find("[data-aos]");
+      sectionDivs.each(function (index) {
+        if (!$(this).attr("data-aos-delay")) {
+          $(this).attr("data-aos-delay", (index + 1) * 100);
+        }
+      });
+    });
+  }
+
+  AOS.init({
+    offset: 20,
+    delay: 50,
+    duration: 750,
+    once: true,
+  });
+
+  const counterUp = window.counterUp.default;
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      const el = entry.target;
+      if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+        counterUp(el, {
+          duration: 3000,
+          delay: 16,
+        });
+        el.classList.add("is-visible");
+      }
+    });
+  };
+  const IO = new IntersectionObserver(callback, { threshold: 1 });
+  const elements = document.querySelectorAll(".counterUp");
+  elements.forEach((el) => IO.observe(el));
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".navbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
